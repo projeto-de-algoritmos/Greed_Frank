39bd314c38e979aeb275938cf7d62056450d000b
Daniel de Sousa Oliveira Melo Veras <daniel_sousa.unb@outlook.com>
2020-10-19 18:12:40 -0300

    :alien: Making basic front-end
    Co-authored-by: Pedro Rodrigues <peddroprp@gmail.com>

---
+++ frank-web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+import Frank from './pages/Frank';
+
+const App: React.FC = () => (
+  <>
+    <GlobalStyle />
+    <Frank />
+  </>
+);
+
+export default App;
---
+++ frank-web/src/pages/Frank/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Frank: React.FC = () => {
+  return (
+    <Container>
+      <h1>Frank Sinatra</h1>
+    </Container>
+  );
+};
+
+export default Frank;
