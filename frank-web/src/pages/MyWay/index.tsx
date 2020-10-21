ca1a9ad0ea082b86ae2faff8b86de8e52db93f6f
Daniel de Sousa Oliveira Melo Veras <daniel_sousa.unb@outlook.com>
2020-10-21 11:45:26 -0300

    :alien: Adding audio my way
    Co-authored-by: Pedro Rodrigues <peddroprp@gmail.com>

--- frank-web/src/pages/MyWay/index.tsx
+++ frank-web/src/pages/MyWay/index.tsx
@@ -1,10 +1,11 @@
 import React, { useCallback, useState } from 'react';
 
+import ReactAudioPlayer from 'react-audio-player';
 import { Container, Footer, Line, Content } from './styles';
-
 import disc from '../../assets/cd.svg';
 import frank from '../../assets/frank.png';
 import api from '../../services/api';
+import MyWayMusic from '../../assets/MyWay.mp3';
 
 const MyWay: React.FC = () => {
   const [isCoded, setIsCoded] = useState<Array<string>>();
@@ -67,6 +68,7 @@ const MyWay: React.FC = () => {
   return (
     <Container>
       <Content>
+        <ReactAudioPlayer src={MyWayMusic} autoPlay loop />
         <img src={frank} alt="frank" />
 
         <span>
