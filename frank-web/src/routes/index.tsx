a1c0718d8e0c58aa6f7f96e379299b29d4fc3eff
Daniel de Sousa Oliveira Melo Veras <daniel_sousa.unb@outlook.com>
2020-10-21 13:05:58 -0300

    :alien: Changing routes

--- frank-web/src/routes/index.tsx
+++ frank-web/src/routes/index.tsx
@@ -4,16 +4,16 @@ import { Switch, Route } from 'react-router-dom';
 import Frank from '../pages/Frank';
 import MyWay from '../pages/MyWay';
 import NewYork from '../pages/NewYork';
-// import StrangersInTheNight from '../pages/StrangersInTheNight';
-// import AllTheWay from '../pages/AllTheWay';
+import StrangersInTheNight from '../pages/StrangersInTheNight';
+import AllTheWay from '../pages/AllTheWay';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Frank} />
     <Route path="/musicOne" component={MyWay} />
     <Route path="/musicTwo" component={NewYork} />
-    {/* <Route path="/musicThree" component={StrangersInTheNight} />
-    <Route path="/musicFour" component={AllTheWay} /> */}
+    <Route path="/musicThree" component={StrangersInTheNight} />
+    <Route path="/musicFour" component={AllTheWay} />
   </Switch>
 );
 
